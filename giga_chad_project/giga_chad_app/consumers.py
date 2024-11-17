4dda768cc4edf51cad7eaf2b2bb045f4ec91d892
craksys <craksys@craksys.win>
2024-11-17 02:14:33 +0100

    xd

--- giga_chad_project/giga_chad_app/consumers.py
+++ giga_chad_project/giga_chad_app/consumers.py
@@ -2,8 +2,41 @@ import asyncio
 import json
 import random
 from channels.generic.websocket import AsyncWebsocketConsumer
+import os
+import sys
+import mne
+import numpy as np
+from datetime import datetime
+from scipy.signal import welch
+huj = "C://Users//craksys//Desktop//git//heroes-of-the-brain-2024//BrainAccessSDK"
+os.add_dll_directory(huj)
+
+import matplotlib.pyplot as plt
+import matplotlib
+import time
+
+from brainaccess.utils import acquisition
+from brainaccess.core.eeg_manager import EEGManager
 
 class BCIConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        matplotlib.use("TKAgg", force=True)
+        eeg = acquisition.EEG()
+        cap: dict = {
+            0: "F3",
+            1: "F4",
+            2: "C3",
+            3: "C4",
+            4: "P3",
+            5: "P4",
+            6: "O1",
+            7: "O2"
+        }
+        device_name = "BA MINI 017"
+        with EEGManager() as mgr:
+            eeg.setup(mgr, device_name=device_name, cap=cap)
+            print(mgr.get_device_info())
     async def connect(self):
         self.room_group_name = "bci_data"
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
