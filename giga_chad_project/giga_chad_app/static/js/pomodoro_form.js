a780cb1b1999040523b04b112d130e9038ea8376
Bartosz Wanot <bartosz.wanot@angrynerds.pl>
2024-11-17 12:03:47 +0100

    dupa

--- giga_chad_project/giga_chad_app/static/js/pomodoro_form.js
+++ giga_chad_project/giga_chad_app/static/js/pomodoro_form.js
@@ -1,9 +1,17 @@
+var focusTime
+var restTime
+var ammount
+var done = 1
 document.getElementById("startSession").addEventListener("click", function () {
+    ammount = document.getElementById('sessionsAmmount').value
+    focusTime = document.getElementById('focusTime').value
+    restTime = document.getElementById('restTime').value 
+    document.querySelector('#total').innerHTML = ammount
+    document.querySelector('#timer').textContent = focusTime + ":00";
+    document.querySelector("#focus-dot").style["animationDuration"] = 60*focusTime+"s"
+    document.querySelector("#chill-dot").style["animationDuration"] = 60*restTime+"s"
+    startTimer(focusTime, switchToChill);
     switchToFocus();
-  
-    var fiveMinutes = 15 ,
-    display = document.querySelector('#timer');
-    startTimer(fiveMinutes, display, switchToChill);
 });
 
 function switchToFocus() {
@@ -28,6 +36,7 @@ function switchToFocus() {
     
     focusBox.style.display = "flex"
     chillBox.style.display = "none"
+    startTimer(focusTime, switchToChill);
 }
 
 function switchToChill() {
@@ -40,7 +49,7 @@ function switchToChill() {
     const chillBox = document.querySelector("#chill-box");
     const focusBox = document.querySelector("#focus-box");
     const title = document.querySelector("#session-title");
-    title.innerHTML = "Chill time!"
+    title.innerHTML = "Rest Period"
 
     gradientHome.style.opacity = "0"; 
     gradientFocus.style.opacity = "0"; 
@@ -51,9 +60,11 @@ function switchToChill() {
 
     focusBox.style.display = "none"
     chillBox.style.display = "flex"
+    startTimer(focusTime, switchToFocus);
 }
 
-function startTimer(duration, display, callback) {
+function startTimer(duration,  callback) {
+    duration *= 60
     var timer = duration, minutes, seconds;
     const interval = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
@@ -62,7 +73,7 @@ function startTimer(duration, display, callback) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        document.querySelector('#timer').textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
             timer = duration;
